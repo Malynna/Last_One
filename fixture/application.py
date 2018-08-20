096210ee63cfeb5c53b0552788e716816c37b7e0
Łukasz2 <Maline@op.pl>
2018-08-20 09:44:18 +0200

    add Class SessionHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
-
+from fixture.session import SessionHelper
 
 
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Chrome()
+        self.session = SessionHelper(self)
