3d9b5ee30eef7a68175e00133d9d5f87ef74bd37
Łukasz2 <Malinex@op.pl>
2018-08-22 17:53:58 +0200

    added go_to_category_phones

--- Pages/frontend_page.py
+++ Pages/frontend_page.py
@@ -6,9 +6,13 @@ class frontend_helper:
     def __init__(self, app):
         self.app = app
 
-    def go_to_category(self):
+    def go_to_category_Lukasz(self):
         wd = self.app.wd
         wd.find_element_by_class_name("category-3").click()
+
+    def go_to_category_XiaomiLepsze(self):
+        wd = self.app.wd
+        wd.find_element_by_class_name("category-1").click()
 
     def check_label_new_on_product(self, product_name):
         wd = self.app.wd
@@ -20,4 +24,8 @@ class frontend_helper:
     def check_shopping_cart(self):
         wd = self.app.wd
         return wd.find_element_by_css_selector("div.title")
+
+    def click_first_product(self):
+        wd = self.app.wd
+        return wd.find_element(By.XPATH, "//h1[@class = 'title']").click
 
--- test/test_buy_products.py
+++ test/test_buy_products.py
@@ -1,7 +1,7 @@
 import pytest
 from fixture.application import application
 from config import config
-
+import time
 
 
 @pytest.fixture
@@ -14,3 +14,4 @@ def app(request):
 def test_buy_products(app):
     app.open_url(config.base_url)
     assert app.front.check_url(config.base_url)
+    app.front.go_to_category_XiaomiLepsze()
