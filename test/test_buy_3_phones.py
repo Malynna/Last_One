6db0984be4f50d207776630284b1ec6d001d52e9
Łukasz2 <Malinex@op.pl>
2018-08-22 10:33:51 +0200

    starting new test

---
+++ test/test_buy_3_phones.py
@@ -0,0 +1,15 @@
+import pytest
+from fixture.application import application
+from config import config
+
+
+
+@pytest.fixture
+def app(request):
+    fixture = application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_buy_products(app):
+    app.open_url(config.base_url)
