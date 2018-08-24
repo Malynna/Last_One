b2c50aabad868b1ac79784db84f7be0e3885e171
Łukasz2 <Malinex@op.pl>
2018-08-24 12:33:03 +0200

    added fill_in_personal_fields to test

--- test/test_buy_products.py
+++ test/test_buy_products.py
@@ -18,3 +18,7 @@ def test_buy_products(app):
     app.front.add_product_to_cart(td.products["second"], td.categories["phones"])
     app.front.add_product_to_cart(td.products["third"], td.categories["phones"])
     app.front.go_to_cart()
+    app.front.fill_in_personal_fields(td.products["tax_id"], td.products["company"], td.products["first_name"],
+                                      td.products["last_name"], td.products["address_1"], td.products["address_2"],
+                                      td.products["postal_code"], td.products["city"], td.products["email"],
+                                      td.products["phone"])
